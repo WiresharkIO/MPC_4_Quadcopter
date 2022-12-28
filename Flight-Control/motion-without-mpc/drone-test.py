5fcde57dca4fb80fd236c7ca772bfc69f473ca2c
Vishal Sivakumar <14985440+WiresharkIO@users.noreply.github.com>
2022-12-28 12:14:43 +0100

    simulation with state and rotation rate equations.

--- Flight-Control/motion-without-mpc/drone-test.py
+++ Flight-Control/motion-without-mpc/drone-test.py
@@ -1,74 +1,46 @@
 ######################################################################################
 import numpy as np
-from numpy import sin as s, cos as c, tan as t
-# from drone_sim.sim.parameters import *
-
 from roblib import *
+######################################################################################
+
 fig = figure()
 ax = Axes3D(fig, auto_add_to_figure=False)
 fig.add_axes(ax)
-print(ax)
+# print(ax)
 
-ech=30
-
+lim=30
 def draw_quadri(X):
     ax.clear()
-    ax.set_xlim3d(-ech, ech)
-    ax.set_ylim3d(-ech, ech)
-    ax.set_zlim3d(0, 1*ech)
+    ax.set_xlim3d(-lim, lim)
+    ax.set_ylim3d(-lim, lim)
+    ax.set_zlim3d(0, 1*lim)
     l=1
-    draw_quadrotor3D(ax, X, A, 5*l)
+    draw_quadrotor3D(ax, X, A, l*5)
 
-def f(x):
-    dx=array([[0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0]]).T
+def motion_dynamics(x):
+    dx=array([[0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0]]).T
     return dx
 
-
 """
-x, y, z,   
-phi(rotation wrt x), psi(rotation wrt y), theta(rotation wrt z),    
-vr, wr  (front, right, down)
+    x,
+    y,
+    z,
+    phi(rotation wrt x),
+    psi(rotation wrt y),
+    theta(rotation wrt z),
+    vr(here r- means frame of robot),
+    wr(front, right, down)
 """
 x = array([[0, 0, -5, 0, 0, 0, 10, 10, 0, 0, 0, 0]]).T
 
 """
-angles for propellers
+    angles for propellers
 """
 A=array([[0, 0, 0, 0]]).T
 
-dt=0.1
-
+dt=0.1 # control the speed of visualization with this parameter
 for t in arange(0, 5, dt):
-    x = x + dt*f(x)
+    x=x+dt*motion_dynamics(x)
     draw_quadri(x)
     pause(0.001)
 pause(1)
-
-
-
-
-######################################################################################
-
-# class Drone:
-#     def __init__(self, x=0, y=0, z=0.5, enable_death=True):
-#         # Position
-#         self.x, self.y, self.z = x, y, z
-#
-#         # Roll Pitch Yaw
-#         self.phi, self.theta, self.psi = 0, 0, 0
-#
-#         # Linear velocities
-#         self.vx, self.vy, self.vz = 0, 0, 0
-#
-#         # Angular Velocities
-#         self.p, self.q, self.r = 0, 0, 0
-#
-#         self.linear_position = lambda: np.array([self.x, self.y, self.z]).reshape(3, 1)
-#         self.angular_position = lambda: np.array([self.phi, self.theta, self.psi]).reshape(3, 1)
-#         self.linear_velocity = lambda: np.array([self.vx, self.vy, self.vz]).reshape(3, 1)
-#         self.angular_velocity = lambda: np.array([self.p, self.q, self.r]).reshape(3, 1)
-
-######################################################################################
-
-
-
